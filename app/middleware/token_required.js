4976ecc8dbba80f1a26da82a14a53bc83c34a2d7
ZhenhuiZhang <924393956@qq.com>
2020-03-15 15:14:46 +0800

    添加并发/多客户请求token刷新的逻辑

--- app/middleware/token_required.js
+++ app/middleware/token_required.js
@@ -15,39 +15,55 @@ module.exports = options => {
       try {
         // 验证token
         decode_token = ctx.service.jwt.jwtVerify(token);
-
-        if (!decode_token || !decode_token.name) {
-          ctx.throw(401, '没有权限，请登录');
-        }
-        if (Date.now() - decode_token.exp * 1000 > 0) {
-          ctx.throw(401, '登录已过期,请重新登录');
+        if (
+          (!decode_token || !decode_token.name) || // token信息不正确
+          Date.now() - decode_token.exp * 1000 > 0// 有效期过了
+        ) {
+          ctx.throw(401, '登录失效,请重新登录');
         }
 
         // 查询用户存在
         const user = await ctx.model.User.findOne({
           name: decode_token.name,
-          accessToken: token,
         }).exec();
-        if (user) {
-          await next();
-        } else {
+        // accessToken: token,
+
+        if (!user || !user.accessToken) {
           ctx.throw('401', '用户信息验证失败');
         }
+        const dbToken = ctx.service.jwt.jwtVerify(user.accessToken);
+        let refreshToken = user.accessToken;
+        /**
+           * 先对比token是否跟db里面user的accessToken是否一样
+           * 1.一样证明验证通过
+           * 2.不一样有可能是短期内，刷新过token，但是有部分客户端/同时发了很多个请求（用相同token同时时请求，但只有一个求情刷新了），需要告诉他们需要用新的token
+           * 3. 如果token还有5分钟过期，则需要更新token
+           */
         if (
-          decode_token &&
-          Date.now() > decode_token.exp * 1000 - 5 * 60 * 1000
-        ) { // 如果response的时候token还有5分钟过期，则需要更新token
-          const user = await ctx.model.User.find({
-            userName: decode_token.name,
-          });
-          const token = ctx.service.jwt.jwtSign(user);
-          ctx.set('authorization', token);
-          ctx.set('token-refresh-at', Date.now());
-          ctx.set('Access-Control-Expose-Headers', 'Authorization,Token-Refresh-At');
+          user.accessToken !== token &&
+          Date.now() - dbToken.iat * 1000 > 5 * 60 * 1000
+        ) {
+          ctx.throw('401', '登录失效,请重新登录');
         }
+
+        if (// 如果response的时候token还有5分钟过期，则需要更新token
+          decode_token &&
+            Date.now() > decode_token.exp * 1000 - 5 * 60 * 1000
+        ) {
+          refreshToken = ctx.service.jwt.jwtSign(user);
+          user.accessToken = refreshToken;
+          user.save();
+        }
+
+        await next();
+
+        ctx.set('authorization', refreshToken);
+        ctx.set('token-refresh-at', Date.now());
+        ctx.set('Access-Control-Expose-Headers', 'Authorization,Token-Refresh-At');
+
       } catch (e) {
         ctx.status = 401;
-        ctx.body = { error: e.message };
+        ctx.body = { error: e.message === 'jwt expired' ? '登录失效,请重新登录' : e.message };
       }
     }
   };
